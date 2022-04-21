c77b14b25a0649746743b2c80a3bbee0499b07b5
bradley.thomas60@googlemail.com <bradley.thomas60@googlemail.com>
2022-04-21 17:16:14 +0100

    showing odd and even numbers of string index

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream> //prints to the console
+#include <vector> // basically arrays
 
 using namespace std;
 
 int main() {
-    string  sentence;
+    string sentence;
     string encrypted_pass;
+    vector <char> password_by_char;
     
     cout << "Enter a word (no spaces): ";
     cin >> sentence;
@@ -15,14 +17,20 @@ int main() {
 
     cout << "-----------------------------------------" << endl;
 
-    for(int i = 0; i < sentence.length(); i++){
-        cout << "the ALTERED ASCII code for " << sentence[i] << " is " << int(sentence[i]) + 5 << " which converts to " << static_cast<char>(int(sentence[i]) + 5) << endl;
+    for(int i = 0; i < sentence.length(); i++) {
+        if (i % 2 != 0) {
+            cout << "Even: " << sentence[i] << endl;
+        } else {
+            cout << "Odd: " << sentence[i] << endl;
+        }
+        
+        
         char encrypted_pass_characters = (static_cast<char>(int(sentence[i]) + 5));
         encrypted_pass += encrypted_pass_characters;
     }
     
     cout << "-----------------------------------------" << endl;
-    
+
     cout << "Your encrypted password is " <<  encrypted_pass << endl;
 
     return 0;
